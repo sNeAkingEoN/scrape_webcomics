9b95be6d96145c1c6c4bf8ef781985d31daf5e69
Anne Königs <anne.koenigs@hotmail.de>
2021-06-09 19:36:36 +0200

    add 4s, but only second chapter

---
+++ webcomics/spiders/ssss.py
@@ -0,0 +1,38 @@
+import os.path
+import re
+import scrapy
+from .base_spiders import FromArchiveSpider
+from ..items import ComicPageHtmlItem
+from pathlib import Path
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.spiders import Rule
+
+class StandStillStaySilenSpider(FromArchiveSpider):
+    name = 'ssss'
+    allowed_domains = ['sssscomic.com']
+    start_urls = ['https://sssscomic.com/?id=archive']
+    links_regex = [r'comic2\.php\?page=\d+'] # nimmt nur die Comics aus dem 2. Adventure, for brevity.
+    rules = (
+        Rule(LxmlLinkExtractor(allow=links_regex), callback='parse_item', follow=False),
+        )
+    max_strip_digits = 3
+    metadata_fields = ['strip_id', 'title', 'url', 'publ_date','last_modified','comment']
+
+    def _create_page_item(self, response): 
+        item = ComicPageHtmlItem()
+        item['name'] = self.name
+        # TODO: nach Teil abgleichen (Adventure 1 oder Adventure 2)
+        # if re.search(r'page-(\d+)', response.url):
+        #     item['strip_id'] = re.search(r'page-(\d+)', response.url).group(1).zfill(self.max_strip_digits)
+        # else:
+        #     item['strip_id'] = '0'
+        item['strip_id'] = '2-' + response.url.split('=')[-1].zfill(max_strip_digits)
+        item['title'] = response.xpath('//title/text()').get().replace(' ', '-') # gibt's offenbar nicht, deshalb wird der sehr generische Titel genommen
+        item['url'] = response.url
+        item['img_url'] = "https://{}/{}".format(self.allowed_domains[0], response.xpath('//img[@class="comicnormal"]/@src').get().strip())
+        item['comment'] = response.xpath('//div[@id="comic_text"]/span[@id="comicdate"]/following-sibling::p').get() # ist noch komplexer, kann z.B. noch img enthalten. Aber einfach `following-sibling::*` geht auch nicht, weil ich dann die ganze Comments-Section mit abgreife
+        item['publ_date'] = response.xpath('//div[@id="comic_text"]/span[@id="comicdate"]/text()').get()
+        return item
+
+
+
