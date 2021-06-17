e7dc3631b54b0d9a807278e1a6e3e072a2a97453
Anne Königs <anne.koenigs@hotmail.de>
2021-06-17 13:08:31 +0200

    add function for saving images

--- webcomics/spiders/base_spiders.py
+++ webcomics/spiders/base_spiders.py
@@ -54,30 +54,29 @@ class FromStartSpider(Spider):
         page_item = self._create_page_item(response)
         link_to_next = self._find_next(response)
         print("***************** URL to Next Page:", link_to_next)
-        # img_request = scrapy.Request(url=page_item['img_url'], callback=self.save_image, cb_kwargs={'page_item': page_item})
+        img_request = scrapy.Request(url=page_item['img_url'], callback=self.save_image, cb_kwargs={'page_item': page_item})
         if link_to_next:
             next_request = scrapy.Request(url=link_to_next, callback=self.parse_page)
-            # return (next_request, img_request)
-            return (next_request, page_item)
+            return (next_request, img_request)
         else:
-            # return img_request
-            return page_item
+            return img_request
 
-    # def save_image(self, response, page_item):
-    #     page_item['last_modified'] = response.headers['last-modified'].decode('utf-8')
-    #     page_item['img_data'] = response.body
-    #     page_item['img_ext'] = response.url.split('.')[-1]
+    def save_image(self, response, page_item):
+        page_item['last_modified'] = response.headers['last-modified'].decode('utf-8')
+        page_item['img_data'] = response.body
+        page_item['img_ext'] = response.url.split('.')[-1]
 
-    #     img_dir = os.path.join(self.settings.get('IMG_BASE_DIRECTORY'), self.name)
-    #     img_file = os.path.join(img_dir,'{}_{}.{}'.format(self.name, page_item['strip_id'], page_item['img_ext']))
+        img_dir = os.path.join(self.settings.get('IMG_BASE_DIRECTORY'), self.name)
+        img_file = os.path.join(img_dir,'{}_{}.{}'.format(self.name, page_item['strip_id'], page_item['img_ext']))
 
-    #     if not os.path.exists(img_dir):
-    #         Path.mkdir(Path(img_dir))
+        if not os.path.exists(img_dir):
+            Path.mkdir(Path(img_dir))
 
-    #     with open(img_file, 'wb') as imgfile:
-    #         imgfile.write(page_item['img_data'])
+        with open(img_file, 'wb') as imgfile:
+            imgfile.write(page_item['img_data'])
 
-    #     return page_item
+        page_item['img_data'] = 'removed' # Easier log output
+        return page_item
 
     def _create_page_item(self, response):
         item = ComicPageHtmlItem()
