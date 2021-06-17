211a98ddf286d851361fc2ae654a5c192dd6d213
Anne Königs <anne.koenigs@hotmail.de>
2021-06-17 13:03:44 +0200

    refactor (with next step in comments)

--- webcomics/spiders/base_spiders.py
+++ webcomics/spiders/base_spiders.py
@@ -4,7 +4,7 @@ import scrapy
 from ..items import ComicPageHtmlItem
 from pathlib import Path
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import Spider, CrawlSpider, Rule
 
 class FromArchiveSpider(CrawlSpider):
     name = 'archive'
@@ -37,20 +37,56 @@ class FromArchiveSpider(CrawlSpider):
         item['img_ext'] = ''
         return item
 
-    def save_image(self, response, page_item):
-        page_item['last_modified'] = response.headers['last-modified'].decode('utf-8')
-        page_item['img_data'] = response.body
-        page_item['img_ext'] = response.url.split('.')[-1]
+class FromStartSpider(Spider):
+    name = ''
+    allowed_domains = []
+    start_urls = []
+    metadata_fields = ['strip_id', 'title', 'url', 'img_url', 'comment', 'publ_date']
 
-        img_dir = os.path.join(self.settings.get('IMG_BASE_DIRECTORY'), self.name)
-        img_file = os.path.join(img_dir,'{}_{}.{}'.format(self.name, page_item['strip_id'], page_item['img_ext']))
+    def parse(self, response):
+        print("+++++++++++++++++++++++First URL downloaded:", response.url)
+        link_to_first = self._find_first(response)
+        print("~~~~~~~~~~~~~~~~~~~ URL to First Page:", link_to_first)
+        yield scrapy.Request(url=link_to_first, callback=self.parse_page) 
 
-        if not os.path.exists(img_dir):
-            Path.mkdir(Path(img_dir))
+    def parse_page(self, response):
+        print("***************** Found page :)", response.url)
+        page_item = self._create_page_item(response)
+        link_to_next = self._find_next(response)
+        print("***************** URL to Next Page:", link_to_next)
+        # img_request = scrapy.Request(url=page_item['img_url'], callback=self.save_image, cb_kwargs={'page_item': page_item})
+        if link_to_next:
+            next_request = scrapy.Request(url=link_to_next, callback=self.parse_page)
+            # return (next_request, img_request)
+            return (next_request, page_item)
+        else:
+            # return img_request
+            return page_item
 
-        with open(img_file, 'wb') as imgfile:
-            imgfile.write(page_item['img_data'])
+    # def save_image(self, response, page_item):
+    #     page_item['last_modified'] = response.headers['last-modified'].decode('utf-8')
+    #     page_item['img_data'] = response.body
+    #     page_item['img_ext'] = response.url.split('.')[-1]
 
-        return page_item
+    #     img_dir = os.path.join(self.settings.get('IMG_BASE_DIRECTORY'), self.name)
+    #     img_file = os.path.join(img_dir,'{}_{}.{}'.format(self.name, page_item['strip_id'], page_item['img_ext']))
 
+    #     if not os.path.exists(img_dir):
+    #         Path.mkdir(Path(img_dir))
 
+    #     with open(img_file, 'wb') as imgfile:
+    #         imgfile.write(page_item['img_data'])
+
+    #     return page_item
+
+    def _create_page_item(self, response):
+        item = ComicPageHtmlItem()
+        item['name'] = self.name
+        # Populate Item
+        return item
+
+    def _find_first(self, response):
+        return # Find link to start in source code
+
+    def _find_next(self,response):
+        return # Fink link to next in source code
--- webcomics/spiders/roomie.py
+++ webcomics/spiders/roomie.py
@@ -1,29 +1,12 @@
 import scrapy
 from ..items import ComicPageHtmlItem
+from .base_spiders import FromStartSpider
 
-class RoomieSpider(scrapy.Spider):
+class RoomieSpider(FromStartSpider):
     name = 'roomie'
     allowed_domains = ['gogetaroomie.com']
     start_urls = ['https://www.gogetaroomie.com/']
-    handle_httpstatus_list = [301]
     metadata_fields = ['strip_id', 'title', 'url', 'img_url', 'comment', 'publ_date']
-
-    def parse(self, response):
-        print("+++++++++++++++++++++++First URL downloaded:", response.url)
-        link_to_first = response.xpath('//a[@class="cc-first"]/@href').get()
-        print("~~~~~~~~~~~~~~~~~~~ URL to First Page:", link_to_first)
-        yield scrapy.Request(url=link_to_first, callback=self.parse_page, dont_filter=False) # Warum auch immer! Achtung! Evtl. an anderen Stellen entfernen
-
-    def parse_page(self, response):
-        print("***************** Found page :)")
-        print(response.url)
-        link_to_next = response.xpath('//a[@class="cc-next"]/@href').get()
-        print("***************** URL to Next Page:", link_to_next)
-        page_item = self._create_page_item(response)
-        if link_to_next:
-            return (scrapy.Request(url=link_to_next, callback=self.parse_page, dont_filter=False), page_item)
-        else:
-            return page_item
 
     def _create_page_item(self, response):
         item = ComicPageHtmlItem()
@@ -36,4 +19,11 @@ class RoomieSpider(scrapy.Spider):
         item['publ_date'] = response.xpath('//div[@class="cc-publishtime"]/text()').get()
         item['img_ext'] = item['img_url'].split('.')[-1]
         return item
+
+    def _find_first(self, response):
+        return response.xpath('//a[@class="cc-first"]/@href').get()
+
+    def _find_next(self,response):
+        return response.xpath('//a[@class="cc-next"]/@href').get()
+
         
